4e44840a9a8606a7b9fae3ce16aaffb5846efb6d
VeryDeadMoth <nix.axius@gmail.com>
2022-12-06 12:27:19 +0100

    AES module skeleton set
    (will be completed later)

---
+++ src/AESModule.java
@@ -0,0 +1,27 @@
+import java.security.Key;
+
+//documentation https://howtodoinjava.com/java/java-security/java-aes-encryption-example/
+// https://www.baeldung.com/java-secure-aes-key
+
+public class AESModule {
+	
+	//generates a random key and calls upon the exportKeyToFile
+	public void generateKey() {
+		
+	}
+	
+	//converts key to JSON file and opens the file explorer
+	public void exportKeyToFile() {
+		
+	}
+	
+	//requests JSON file to convert into a key it returns
+	public Key importKeyFromFile() {
+		return null;
+	}
+	
+	public static void main(String[] args) {
+
+	}
+
+}
